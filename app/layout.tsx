a76c91d7b33543d52eedd32b460ad71754213657
Harshit Vijay <93333205+Coder-Harshit@users.noreply.github.com>
2024-11-06 15:04:06 +0530

    unused variable comment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,20 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import Head from 'next/head';
 import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+// import localFont from "next/font/local";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -27,6 +29,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
+      {/* <Head>
+        <link rel="preload" href="./fonts/Ge" />
+      </Head> */}
       <body className={`${inter.className} h-full`}>
         <div className="min-h-full">
           {children}

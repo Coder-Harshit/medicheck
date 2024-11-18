05d4aa0d634c6c799d4e78df4a36d06e5fe80bcd
Harshit Vijay <93333205+Coder-Harshit@users.noreply.github.com>
2024-11-18 17:39:48 +0530

    multi-drop working

--- components/multi-select-dropdown.tsx
+++ components/multi-select-dropdown.tsx
@@ -442,7 +442,7 @@ export const MultiSelect = React.forwardRef<
                             : "opacity-50 [&_svg]:invisible"
                         )}
                       >
-                        <CheckIcon className="h-4 w-4" />
+                        <CheckIcon className="h-4 w-4 bg-green-600" />
                       </div>
                       {option.icon && (
                         <option.icon className="mr-2 h-4 w-4 text-muted-foreground" />
